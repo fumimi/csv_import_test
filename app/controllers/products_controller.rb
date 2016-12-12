b0422b1b7e6a8b9d75e40d21c50373f263abfdcc
fumimi <info@fumimi.jp>
2016-12-12 12:20:11 +0900

    first commit

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,15 @@
+class ProductsController < ApplicationController
+  # def index
+  # end
+
+  def index
+    @products = Product.all
+  end
+
+  def import
+    # fileはtmpに自動で一時保存される
+    Product.import(params[:file])
+    redirect_to root_url, notice: "商品を追加しました。"
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CsvImportTest::Application.config.secret_key_base = 'd9642adc195e88877d02b03f24bb3abe8701dfa11dc593a650ce07a1857e7f7dae043761648fda47bdb5f9096d5580b6327aa285f91d5b98ccd585df257bb9ef'
